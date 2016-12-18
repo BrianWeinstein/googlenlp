ef8ba08e9ff69ed4b8543d374e8bab8016e70316
BrianWeinstein <bweinstein02@gmail.com>
2016-12-18 13:25:59 -0500

    update post method documentation

--- R/request.R
+++ R/request.R
@@ -2,9 +2,9 @@
 #' @importFrom httr POST user_agent accept_json http_type content http_error status_code
 NULL
 
-#' Send a POST request to the Google Cloud Natural Language API and retrieve the results
+#' Send a POST request to the Google Cloud Natural Language API
 #' 
-#' TODO: Description
+#' Send a POST request to the Google Cloud Natural Language API and retrieve the results.
 #'
 #' @param api_key Your API key, from \url{https://console.cloud.google.com/apis/credentials}. Either enter the API key directly, or access it via \code{gcnlp_key()}.
 #' @param text_body The text string to send to the API.
@@ -15,12 +15,12 @@ NULL
 #' @examples
 #' gcnlp_post(api_key = gcnlp_key(),
 #'            text_body = "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.
-#'                        Sundar Pichai said in his keynote that users love their new Android phones.",
+#'                         Sundar Pichai said in his keynote that users love their new Android phones.",
 #'            extract_syntax = TRUE,
 #'            extract_entities = TRUE,
 #'            extract_document_sentiment = TRUE)
 #' @return
-#' TODO: RETURN VALUE
+#' A list containing two elements: [1] \code{content} includes the parsed response, and contains the \code{sentences}, \code{tokens}, \code{entities}, \code{documentSentiment}, \code{language} results specified in the request. [2] \code{raw_response} contains the raw response from the API.
 #'
 #' @export
 gcnlp_post <- function(api_key = gcnlp_key(),
@@ -31,7 +31,7 @@ gcnlp_post <- function(api_key = gcnlp_key(),
   
   url <- paste0("https://language.googleapis.com/v1/documents:annotateText/?key=", api_key)
   
-  response <- POST(url = url,
+  raw_response <- POST(url = url,
                    config = c(user_agent("http://github.com/brianweinstein/googlenlp"),
                               accept_json()),
                    body = list(document=list(type="PLAIN_TEXT",
@@ -43,18 +43,18 @@ gcnlp_post <- function(api_key = gcnlp_key(),
                    encode = "json")
   
   # check if API returned JSON
-  if (http_type(response) != "application/json") {
+  if (http_type(raw_response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
   
-  # parse the response
-  parsed <- jsonlite::fromJSON(content(response, "text"), simplifyVector = FALSE)
+  # parse the raw_response
+  parsed <- jsonlite::fromJSON(content(raw_response, "text"), simplifyVector = FALSE)
   
   # check the status
-  if (http_error(response)) {
+  if (http_error(raw_response)) {
     stop(
       paste0(
-        "Google Cloud Natural Language API request failed ", "[", status_code(response), "]\n", 
+        "Google Cloud Natural Language API request failed ", "[", status_code(raw_response), "]\n", 
         parsed$error$message, "\n",
         parsed$error$details[[1]]$links[[1]]$url
       ),
@@ -65,7 +65,7 @@ gcnlp_post <- function(api_key = gcnlp_key(),
   structure(
     list(
       content = parsed,
-      response = response
+      raw_response = raw_response
     ),
     class = "google_natural_language_api"
   )
